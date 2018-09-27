11aeb0db1a27021080fe65e3d5ed594c3d7a7295
sk0rn <hyperthrone@gmail.com>
2018-09-27 22:28:37 +1100

    add Maven framework support

---
+++ src/main/java/DemoApp.java
@@ -0,0 +1,29 @@
+import utils.SourceRipper;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class DemoApp {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String pathIn = "D:\\dev_edu\\STC13_HT\\Lab01_FileParser\\src\\main\\java\\datain\\";
+        String pathOut = "D:\\dev_edu\\STC13_HT\\Lab01_FileParser\\src\\main\\java\\dataout\\result.dat";
+        String[] searchedWords = {"hello", "cruel", "world"};
+        String[] sources = null;
+        File dir = new File(pathIn);
+        FilenameFilter filter = (dir1, name) -> name.toLowerCase().matches("([\\s\\w*])+(\\.)+([\\w]+)$");
+        if (dir.exists()) {
+            String[] f = dir.list(filter);
+            sources = new String[f.length];
+            for (int i = 0; i < f.length; i++) { ;
+                sources[i] = pathIn + f[i];
+            }
+        }
+
+        SourceRipper sr = new SourceRipper(4);
+        sr.getOccurencies(sources, searchedWords, pathOut);
+
+
+    }
+}
